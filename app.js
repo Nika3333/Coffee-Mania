8aee4f869d0c84296789813be56037819b57466a
Nika3333 <103197037+Nika3333@users.noreply.github.com>
2023-09-24 23:07:39 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+// Menu icon 
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+  toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+  })
+
+
+// See more
+
+  const wrapperCards = document.querySelector('.wrapper-cards')
+  const seeMore = document.querySelector('.see-more')
+
+  const showWrapperCards = seeMore.addEventListener('click', () =>  {
+    wrapperCards.classList.toggle('active')
+
+    // See more text changer
+    
+      if (seeMore.innerHTML === 'See More') {
+        seeMore.innerHTML = 'Hide';
+        return;
+      } else {
+        seeMore.innerHTML = 'See More'
+      }
+
+  })
+
