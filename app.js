9b14ceeae94b874f6e9df8a18b4265d5225b41a9
Nika3333 <103197037+Nika3333@users.noreply.github.com>
2023-09-27 20:49:31 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
+// Variables
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const wrapperCards = document.getElementsByClassName('wrapper-cards')[0]
+const seeMore = document.getElementsByClassName('see-more')[0]
+
 // Menu icon 
 
-  const toggleButton = document.getElementsByClassName('toggle-button')[0]
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-  toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-  })
-
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
 
 // See more
 
-  const wrapperCards = document.getElementsByClassName('wrapper-cards')[0]
-  const seeMore = document.getElementsByClassName('see-more')[0]
- 
+seeMore.addEventListener('click', () =>  {
+wrapperCards.classList.toggle('active')
 
-  seeMore.addEventListener('click', () =>  {
-  wrapperCards.classList.toggle('active')
-
-    // See more text changer
+  // See more text changer
+  
+    if (seeMore.innerHTML === 'See More') {
+      seeMore.innerHTML = 'Hide';
+      return;
+    } else {
+      seeMore.innerHTML = 'See More'
+    }
     
-      if (seeMore.innerHTML === 'See More') {
-        seeMore.innerHTML = 'Hide';
-        return;
-      } else {
-        seeMore.innerHTML = 'See More'
-      }
+})
 
-  })
+// More button toggle 
+
+seeMore.onclick = function myFunction() {
+  const moreButton = document.getElementsByClassName('more')[0]
+  if (moreButton.style.display === "block") {
+    moreButton.style.display = "none";
+  } else {
+    moreButton.style.display = "block";
+  }
+}
+
+
+
+
 
